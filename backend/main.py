823cb391d445f17f57199e26fd203795f64643c2
Rajashekarreddy111 <rajashekharreddyyerruva@gmail.com>
2026-08-07 20:08:18 +0530

    stage-1 completed

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="NetworkIQ Backend",
+    version="1.0.0"
+)
 
 @app.get("/")
-def home():
-    return {"message": "Hello"}
+def root():
+    return {
+        "message": "NetworkIQ Backend Running"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "ok"
+    }
